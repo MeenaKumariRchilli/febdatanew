fb20ef73baa29bcf9e44fa003e4eae2ac7d4e261
MeenaKumari <meena@rchilli.com>
2017-02-07 09:26:09 +0530

    first output is by deve 1

---
+++ src/ca/demo/datademonew.java
@@ -0,0 +1,12 @@
+package ca.demo;
+
+public class datademonew {
+
+	public static void main(String[] args) {
+		String [] names={"meena","pihu","shewta"};
+		for(int i=0; i<names.length;i++){
+			System.out.println("Output is :" +names[i]);
+		}
+	}
+
+}
