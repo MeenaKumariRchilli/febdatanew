186bd2adfe569fcabeb6063b52dd130dbf7213da
MeenaKumari <meena@rchilli.com>
2017-02-07 09:32:29 +0530

    2nd time editing data

--- src/ca/demo/datademonew.java
+++ src/ca/demo/datademonew.java
@@ -3,9 +3,9 @@ package ca.demo;
 public class datademonew {
 
 	public static void main(String[] args) {
-		String [] names={"meena","pihu","shewta"};
-		for(int i=0; i<names.length;i++){
-			System.out.println("Output is :" +names[i]);
+		String [] names={"meena","pihu","shewta","mishu","rajat"};
+		for(String name :names){
+			System.out.println("Output is :" +names);
 		}
 	}
 
