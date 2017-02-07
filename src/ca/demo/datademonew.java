ca174dcf596c77cf105894654e50de1f10048f7f
MeenaKumari 2 <meena2@rchilli.com>
2017-02-07 09:34:21 +0530

    data edited by 2nd dev

--- src/ca/demo/datademonew.java
+++ src/ca/demo/datademonew.java
@@ -3,9 +3,10 @@ package ca.demo;
 public class datademonew {
 
 	public static void main(String[] args) {
-		String [] names={"meena","pihu","shewta"};
+		String [] names={"meena","pihu","shewta","harman mam"};
 		for(int i=0; i<names.length;i++){
 			System.out.println("Output is :" +names[i]);
+		//	System.out.println("Output is :" +names[i]);	
 		}
 	}
 
